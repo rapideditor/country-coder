b2a661ac0188f528ebb73b3666f7b436cee42396
Bryan Housel <bhousel@gmail.com>
2025-10-21 11:22:40 -0400

    Switch from HTMLRewriter to string replace for rewriting CDN URLs
    HTMLRewriter is ok for picking out the `src` of a script tag,
    but I found for other projects I want to rewrite links in other
    places, like img tags or strings within the actual scripts.
    
    This switches to a more generic string replacement approach that will
    find CDN URLs in those other places too.
    
    String replacement should be ok to do here because it will only
    be used on html files that easily fit in memory, and none of
    these projects are very complicated.

--- scripts/server.ts
+++ scripts/server.ts
@@ -3,21 +3,22 @@ import { styleText } from 'bun:util';
 const project = 'country-coder';
 const hostname = '127.0.0.1';
 const port = 8080;
-const matchCDN = new RegExp(`jsdelivr.*${project}.*(\/dist.*)$`, 'i');
+const matchCDN = new RegExp(`(['"\`])(https?:\/\/cdn.jsdelivr.*${project}.*\/)(dist.*["'\`])`, 'gi');
 
 
 // Replace urls for CDN `dist/*` files with local `dist/*` files.
 // e.g. 'https://cdn.jsdelivr.net/npm/path/to/dist/file.min.js' -> '/dist/file.js'
-const rewriter = new HTMLRewriter().on('script', {
-  element(script) {
-    const src = script.getAttribute('src');
-    const cdn = src?.match(matchCDN);
-    if (cdn) {
-      const local = cdn[1].replace('.min', '');
-      script.setAttribute('src', local);
-    }
-  }
-});
+function replaceCDNPath(s: string): string {
+  return s.replaceAll(matchCDN, replacer);
+}
+
+// The replacer function for replaceAll:
+// p1 = "           - begin string
+// p2 = cdn url     - removed
+// p3 = dist/file"  - 'dist' + file (if any) + end string
+function replacer(match: string, p1: string, p2: string, p3: string): string {
+  return p1 + p3.replace('.min', '');
+}
 
 
 // Start the server!
@@ -33,35 +34,35 @@ const server = Bun.serve({
     const last = path.length - 1;
     console.log(styleText('yellowBright', `${req.method}:  ${url.pathname}`));
 
-    path[0] = 'docs';            // leading '/' -> serve from './docs'
-    if (path[last] === '') {     // no filename, default to 'index.html'
+    path[0] = 'docs';          // leading '/' -> serve from './docs/*'
+    if (path[last] === '') {   // no filename, default to 'index.html'
       path[last] = 'index.html';
     }
     if (path[1] === 'dist') {  // Also allow serving files from './dist/*'
       path.shift();            // (remove leading 'docs')
     }
 
-    const filePath = './' + path.join('/');
+    const filepath = './' + path.join('/');
 
     try {
-      const file = Bun.file(filePath);
+      const file = Bun.file(filepath);
       if (await file.exists()) {
-        console.log(styleText('greenBright', `200:  Found → '${filePath}'`));
+        console.log(styleText('greenBright', `200:  Found → '${filepath}'`));
 
-        if (/html$/.test(filePath)) {
-          const content = rewriter.transform(await file.text());
-          return new Response(content, { headers: { 'content-type': 'text/html' }});
+        if (/html$/.test(filepath)) {
+          const content: string = await file.text();
+          return new Response(replaceCDNPath(content), { headers: { 'content-type': 'text/html' }});
         } else {
           return new Response(file);
         }
       }
     } catch (error) {
       // Handle potential errors during file access
-      console.error(`Error serving file: ${filePath}`, error);
+      console.error(`Error serving file: ${filepath}`, error);
     }
 
     // If file not found or error, return 404
-    console.log(styleText('redBright', `404:  Not Found → '${filePath}'`));
+    console.log(styleText('redBright', `404:  Not Found → '${filepath}'`));
     return new Response('Not Found', { status: 404 });
   }
 });
